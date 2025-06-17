b77c26ce23085c7d63956a240be35163b561776e
rayhanalmim <rayhanalmim1@gmail.com>
2025-06-17 17:35:08 +0600

    updated cors issue

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || ['http://localhost:3000', 'https://your-domain.com'],
+  origin: process.env.FRONTEND_URL || ['http://localhost:3000', 'https://client-neon-iotddda.vercel.app', 'https://rebel-ape-client-with-dashboard.vercel.app'],
   credentials: true
 }));
 
